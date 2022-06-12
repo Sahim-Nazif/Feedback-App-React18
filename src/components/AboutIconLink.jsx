67f05f85489bf709f6959f7c75e1732c13131733
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-06-12 10:03:20 -0400

    created icon link component

---
+++ src/components/AboutIconLink.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Fa500Px, FaQuestion} from 'react-icons/fa'
+
+
+const AboutIconLink = () => {
+  return (
+    <div className='about-link'>
+        <FaQuestion size={30}/>
+    </div>
+  )
+}
+
+export default AboutIconLink
